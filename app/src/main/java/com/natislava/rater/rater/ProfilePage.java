37ca6c59bafe790329298996a9f348c503e522f3
netanel <sababish@gmail.com>
2016-04-01 11:47:02 +0300

    profilepage created cosmetics

---
+++ app/src/main/java/com/natislava/rater/rater/ProfilePage.java
@@ -0,0 +1,16 @@
+package com.natislava.rater.rater;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by temp on 01/04/2016.
+ */
+public class ProfilePage extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile_screen);
+
+    }
+}
