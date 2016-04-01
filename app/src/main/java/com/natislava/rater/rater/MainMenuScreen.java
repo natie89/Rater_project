f41df3d6afe12d4f5918fa1f488e513d9cce4339
netanel <sababish@gmail.com>
2016-04-01 14:20:41 +0300

    main menu cosmetics

---
+++ app/src/main/java/com/natislava/rater/rater/MainMenuScreen.java
@@ -0,0 +1,16 @@
+package com.natislava.rater.rater;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by temp on 01/04/2016.
+ */
+public class MainMenuScreen extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_menu_screen);
+
+    }
+}
